0f9d56184b59fafc0cf09a38e9c51011dcbe7610
Caroline Cange <carolinecang@gmail.com>
2019-07-11 12:28:26 -0700

    done with the console game tictactoe

--- tictactoe.js
+++ tictactoe.js
@@ -1,25 +1,45 @@
+
 var game = function() {
   //private
+
   var winner;
   var gameOver = false;
-  var count = 0;
+  var count = 1;
+
   const checkForGameOver = function() {
     if (gameOver === true) {
-      console.log('Game Over');
+      console.log("Game Over");
       return;
     }
-  }
+  };
   const isEqual = function(arr) {
-    for (var i = 0; i+1 <= 3; i++) {
-      if (arr[i] === arr[i+1]) {
-        winner = arr[0];
-      } else if (arr[i] === ' ') {
+    for (var i = 0; i < 2; i++) {
+      if (arr[i] !== arr[i + 1]) {
         return false;
-      } else {
+      } else if (arr[i] === " ") {
         return false;
       }
     }
-  }
+    winner = arr[0];
+    return true;
+  };
+
+  const checkInput = function(position, char) {
+    if (position > 8) {
+      console.log("please enter position between 1 to 9.");
+      return;
+    } else if (numbers[position] !== " ") {
+      console.log("spot has already token.");
+      return;
+    }
+    if (char === "x" || char === "X") {
+      numbers[position] = "X";
+    } else if (char === "o" || char === "O") {
+      numbers[position] = "O";
+    } else {
+      console.log("please enter a valid letter! X or O");
+    }
+  };
 
   const findWinner = function() {
     if (
@@ -34,28 +54,22 @@ var game = function() {
       //check diagonal
       isEqual([numbers[0], numbers[4], numbers[8]]) ||
       isEqual([numbers[2], numbers[4], numbers[6]])
-
     ) {
       return true;
     }
-  }
-  const checkInput = function(position, char) {
-    
-  }
+  };
 
-
-  return{
+  return {
     showBoard: function(init) {
       //show board with numbers if just started
       if (init) {
-        var numbers = [1,2,3,4,5,6,7,8,9];
-        console.log('Welcome to play tictactoe');
-
+        numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        console.log("Welcome to play tictactoe");
       }
-      var row1 = numbers[0] + ' | ' + numbers[1] + ' | ' + numbers[2] + '\n';
-      var row2 = numbers[3] + ' | ' + numbers[4] + ' | ' + numbers[5] + '\n';
-      var row3 = numbers[6] + ' | ' + numbers[7] + ' | ' + numbers[8] + '\n';
-      var line = '--|---|--' + '\n';
+      var row1 = numbers[0] + " | " + numbers[1] + " | " + numbers[2] + "\n";
+      var row2 = numbers[3] + " | " + numbers[4] + " | " + numbers[5] + "\n";
+      var row3 = numbers[6] + " | " + numbers[7] + " | " + numbers[8] + "\n";
+      var line = "--|---|--" + "\n";
       console.log(row1);
       console.log(line);
       console.log(row2);
@@ -63,14 +77,35 @@ var game = function() {
       console.log(row3);
       //blank board
       if (init) {
-        numbers = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+        numbers = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
       }
     },
     place: function(position, choice) {
-
+      if (gameOver === true) {
+        checkForGameOver();
+        return;
+      }
+      //check for user input
+      checkInput(position, choice);
+      count++;
+      if (findWinner()) {
+        this.showBoard();
+        console.log("the winner is " + winner + "!");
+        gameOver = true;
+      } else if (count > 9) {
+        console.log("tie");
+      } else {
+        this.showBoard();
+      }
+    },
+    newGame: function() {
+      numbers = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+      count = 1;
+      gameOver = false;
+      this.showBoard(true);
     }
-  }
-}
+  };
+};
 
 var tictactoe = game();
 //start
